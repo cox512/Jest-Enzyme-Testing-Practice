a33e6b6cf3242ed0a9f7b0d6ae6586230c8a6bda
Jacob Cox <jacob@championofvictory.com>
2020-12-11 23:37:35 -0600

    Add Thunk for guessWord piece of State

--- src/reducers/guessedWordsReducer.js
+++ src/reducers/guessedWordsReducer.js
@@ -1,3 +1,10 @@
-export default (state, action) => {
-  return null;
+import { actionTypes } from "../actions";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
