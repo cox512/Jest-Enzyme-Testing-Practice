1426c32ec8546c352d218ae908c281ed298461b7
Jacob Cox <jacob@championofvictory.com>
2020-12-16 20:59:40 -0600

    wrote secret word tests, action creators, and reducer

--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,3 +1,12 @@
+import { actionTypes } from "../actions";
+
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = null, action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.SET_SECRET_WORD:
+      return action.payload;
+
+    default:
+      return state;
+  }
 };
