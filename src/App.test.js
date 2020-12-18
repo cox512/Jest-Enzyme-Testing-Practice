eaa86e9c734d6f38571038f56ec4bc5543b4eb9d
Jacob Cox <jacob@championofvictory.com>
2020-12-17 19:20:13 -0600

    Add getSecretWord action creator on App mount

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,58 @@
-import App from "./App";
+import React from "react";
+import App, { UnconnectedApp } from "./App";
 
-test("renders learn react link", () => {});
+import { shallow } from "enzyme";
+import { storeFactory } from "../test/testUtils";
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe("redux props", () => {
+  test("has 'success' piece of state as prop", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProps = wrapper.instance().props.success;
+    expect(successProps).toBe(success);
+  });
+  test("has secretWord piece of state as prop", () => {
+    const secretWord = "party";
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test("Has access to the guessedWords state", () => {
+    const guessedWords = [{ guessedWord: "train", letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProps = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProps).toBe(guessedWords);
+  });
+  test("getSecretWord action creator is a function on the props", () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
+});
+
+test('"getSecretWord" runs on app mount', () => {
+  const getSecretWordMock = jest.fn();
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: false,
+    guessedWordsProps: [],
+  };
+  //set up app component with getSecretWordMock as the getSecretWord prop
+  const wrapper = shallow(<UnconnectedApp {...props} />);
+
+  //run Lifecycle method
+  wrapper.instance().componentDidMount();
+
+  //check to see if mock ran
+  const getSecretWordsCallCount = getSecretWordMock.mock.calls.length;
+
+  expect(getSecretWordsCallCount).toBe(1);
+});
